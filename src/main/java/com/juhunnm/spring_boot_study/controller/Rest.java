0f875fd1aad7789cee5d974857609ab853913a93
juhun <minjuhun1@gmail.com>
2024-04-24 13:00:35 +0900

    first_commit

---
+++ src/main/java/com/juhunnm/spring_boot_study/controller/Rest.java
@@ -0,0 +1,48 @@
+package com.juhunnm.spring_boot_study.controller;
+
+import com.juhunnm.spring_boot_study.dto.ItemDto;
+import com.juhunnm.spring_boot_study.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Rest {
+
+    @GetMapping("/api/data")
+    public String fetchData() {
+        return "Data from Spring Boot";
+    }
+}
+//@RestController
+//@Slf4j
+//public class Rest {
+//    @GetMapping("/dummy")
+//    public String dummy() {
+//        log.info("dummy");
+//        return "{}";
+//    }
+//    @GetMapping("/dummy2")
+//    public String dummy2() {
+//        log.info("dummy2");
+//        return "dummy2";
+//    }
+//    @GetMapping("/member")
+//    public String getMember(@RequestParam("empNo") String empNo,@RequestParam("year") int year) {
+//        log.info("empNo : {}",empNo);
+//        log.info("year : {}",year);
+//        return "ok";
+//    }
+//    @GetMapping("/company/{id}")
+//    public String getCompany(@PathVariable("id") String id) {
+//        log.info("id : {}",id);
+//        return "ok";
+//    }
+//    @PostMapping("/item")
+//    public ResponseDto registerItem(@RequestBody ItemDto item){
+//        log.info("item : {}",item);
+//        ResponseDto responseDto = new ResponseDto();
+//        responseDto.setMessage("success");
+//        return responseDto;
+//
+//    }
+//}
---
+++ src/main/java/com/juhunnm/spring_boot_study/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.juhunnm.spring_boot_study.dto;
+
+public class ResponseDto {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
