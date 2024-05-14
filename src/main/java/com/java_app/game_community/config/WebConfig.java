47c8f25d10a22c500b974c0abcaedfa49acdbbf7
juhun <minjuhun1@gmail.com>
2024-05-15 00:30:44 +0900

    add new feature

---
+++ src/main/java/com/java_app/game_community/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.java_app.game_community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 리액트 서버 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true);
+    }
+}
---
+++ src/test/java/com/java_app/game_community/GameCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_app.game_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
